60e18f42dae3d6512fd58d3e382f783f44dce6f3
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2021-04-25 15:01:07 -0300

    terminando aula 4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,6 +11,8 @@ import { api } from '../services/api'
 import { ConvertDurationToTimeString } from '../utils/ConvertDurationToTimeString'
 
 import styles from './home.module.scss'
+import { useContext } from 'react'
+import { PlayerContext } from '../contexts/PlayerContext'
 
 type Episode = {
   id: string
@@ -29,6 +31,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext)
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -55,7 +59,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio"/>
                 </button>
               </li>
